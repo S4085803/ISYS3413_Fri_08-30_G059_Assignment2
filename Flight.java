70f3f7f06c56685d44ed9327d4cac184e2746e38
Sowndharyaa <you@example.com>
2025-09-19 16:27:04 +1000

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,3 +1,25 @@
+// Author: Sowndharyaa (s4090884)
+// Contribution: Flight class
+
+import java.util.List;
+
 public class Flight {
-    
+    private String flightID;
+    private String airlineName;
+    private String departureTime;
+    private String arrivalTime;
+    private int duration;
+    private float price;
+    private int numOfStops;
+    private List<String> stopoverLocations;
+
+    public int calculateDuration() {
+        // Placeholder
+        return 0;
+    }
+
+    public int calculateFlightTicket() {
+        // Placeholder
+        return 0;
+    }
 }
