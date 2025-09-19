36b82a29413f4e7d4513ce5fbb6a8ea42ca7fff7
Sowndharyaa <you@example.com>
2025-09-19 16:59:13 +1000

    update ServiceProvider.java

--- ServiceProvider.java
+++ ServiceProvider.java
@@ -4,14 +4,8 @@
 import java.util.List;
 
 public class ServiceProvider {
-    private String providerID;
     private String providerName;
-    private String contactInfo;
     private String endpointURL;
-
-    public String getProviderDetails() {
-        return "";
-    }
     
     public List<Flight> getRealTimeFlightData() {
         return null;
